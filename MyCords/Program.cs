b36b5ed91bcfe7ec8d3c38abf23c4b4e12dfee1a
Estefanny <i1412121@continental.edu.pe>
2018-11-01 19:24:26 -0500

    convertiendo de ingles a español

--- MyCords/Program.cs
+++ MyCords/Program.cs
@@ -16,36 +16,36 @@ namespace MyCords
     {
         static void Main(string[] args)
         {
-            bool toContinue = true;
+            bool continuar = true;
 
             Console.WriteLine("Hola, ingresa coordenadas (latitud, longitud) o x para salir:");
             Console.WriteLine("Ejemplo: 12.3444,14.2333");
             List<Coord> coordenadas = new List<Coord>();
-            while (toContinue)
+            while (continuar)
             {
-                String input = Console.ReadLine();
-                if (input.ToLower().Equals("x"))
+                String entrar = Console.ReadLine();
+                if (entrar.ToLower().Equals("x"))
                 {
-                    toContinue = false;
+                    continuar = false;
                     continue;
                 }
-                if (input.Split(',').Length != 2)
+                if (entrar.Split(',').Length != 2)
                 {
                     continue;
                 }
                 // todo lo que es correcto
                 Double latitud;
                 Double longitud;
-                Coord mycoord = new Coord()
+                Coord Mi_Coordinada = new Coord()
                 {
-                    latitud = Double.TryParse(input.Split(',')[0], out latitud) ? latitud:0,
-                    longitud = Double.TryParse(input.Split(',')[1], out longitud) ? longitud : 0,
+                    latitud = Double.TryParse(entrar.Split(',')[0], out latitud) ? latitud:0,
+                    longitud = Double.TryParse(entrar.Split(',')[1], out longitud) ? longitud : 0,
                 };
 
-                coordenadas.Add(mycoord);
+                coordenadas.Add(Mi_Coordinada);
             }
 
-            Console.WriteLine("Good bye... tu has ingresado: {0} coordenadas", coordenadas.Count);
+            Console.WriteLine("Gracias por utilizar nuestro app... tu has ingresado: {0} coordenadas", coordenadas.Count);
             Console.ReadKey();
         }
     }
